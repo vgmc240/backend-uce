88824cc1c325d6939a91337aee80ea45a717990e
vgmc240 <vitorgmc240@gmail.com>
2025-05-06 20:03:51 -0300

    criando nova rota

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/", (req, res)=>{
     return res.json({message:"To funcionando"})
 })
 
+app.get("/rota-teste", (req, res)=>{
+    return res.json({message:"To funcionando"})
+})
+
 app.listen(3333)
